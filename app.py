da7c9711526f99c1fcfd4c88a3c11d6581fcb3dc
LucasEBotelho <lucasone2050@hotmail.com>
2025-03-22 23:38:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -39,15 +39,15 @@ def cadastrar_produto():
     produtos_df = carregar_produtos()
     
     # Opção de escolher um produto
-    produto_selecionado = st.selectbox("Escolha o produto", produtos_df['Nome Produto'])
+    produto_selecionado = st.selectbox("Escolha o produto", produtos_df['Nome Produto'], key="produto_selectbox")
     
     # Se o produto não estiver na lista, adicionar um novo
     if produto_selecionado == "Adicionar Novo Produto":
-        codigo_produto = st.text_input("Código do Produto")
-        nome_produto = st.text_input("Nome do Produto")
+        codigo_produto = st.text_input("Código do Produto", key="codigo_produto")
+        nome_produto = st.text_input("Nome do Produto", key="nome_produto")
         
         # Salvar o novo produto
-        if st.button("Salvar Novo Produto"):
+        if st.button("Salvar Novo Produto", key="salvar_produto"):
             novo_produto = {"Codigo Produto": codigo_produto, "Nome Produto": nome_produto}
             produtos_df = produtos_df.append(novo_produto, ignore_index=True)
             produtos_df.to_excel("produtos.xlsx", index=False)
@@ -56,19 +56,19 @@ def cadastrar_produto():
             st.session_state.produto_selecionado = nome_produto
     else:
         # Preencher os dados do estoque
-        data_validade = st.date_input("Data de Validade", min_value=datetime.today())
-        unidade = st.selectbox("Unidade", ["Betelitas", "Lojinha", "Commulter"])
-        quantidade = st.number_input("Quantidade", min_value=0)
-        valor = st.number_input("Valor", min_value=0.0, format="%.2f")
-        observacao = st.text_input("Observação")
-        promocao = st.checkbox("Promoção?")
-        perda = st.checkbox("Perda?")
+        data_validade = st.date_input("Data de Validade", min_value=datetime.today(), key="data_validade")
+        unidade = st.selectbox("Unidade", ["Betelitas", "Lojinha", "Commulter"], key="unidade")
+        quantidade = st.number_input("Quantidade", min_value=0, key="quantidade")
+        valor = st.number_input("Valor", min_value=0.0, format="%.2f", key="valor")
+        observacao = st.text_input("Observação", key="observacao")
+        promocao = st.checkbox("Promoção?", key="promocao")
+        perda = st.checkbox("Perda?", key="perda")
         
         valor_promocional = 0.0
         perda_pos_promocao = 0.0
         
         if promocao:
-            valor_promocional = st.number_input("Valor Promocional", min_value=0.0, format="%.2f")
+            valor_promocional = st.number_input("Valor Promocional", min_value=0.0, format="%.2f", key="valor_promocional")
             # Cálculo da Perda Pós Promoção
             perda_pos_promocao = (valor - valor_promocional) * quantidade
         
@@ -92,7 +92,7 @@ def cadastrar_produto():
         }
         
         # Botão para salvar os dados no estoque
-        if st.button("Salvar Cadastro"):
+        if st.button("Salvar Cadastro", key="salvar_cadastro"):
             salvar_estoque(dados_estoque)
             st.success("Produto cadastrado com sucesso!")
             # Armazenar dados no session_state para persistir após re-render
@@ -105,10 +105,11 @@ st.title("Controle de Validade dos Produtos")
 if 'produto_selecionado' not in st.session_state:
     st.session_state.produto_selecionado = None
 
+# Definindo layout para os botões
 col1, col2 = st.columns(2)
 
 with col1:
-    if st.button("Consultar Produtos"):
+    if st.button("Consultar Produtos", key="consultar_produtos"):
         # Mostrar produtos cadastrados de forma mais bonita
         estoque_df = carregar_estoque()
         
@@ -125,5 +126,5 @@ with col1:
         }))
 
 with col2:
-    if st.button("Cadastrar Novo Produto"):
+    if st.button("Cadastrar Novo Produto", key="cadastrar_produto"):
         cadastrar_produto()
